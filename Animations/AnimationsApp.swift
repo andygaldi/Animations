b94def637cf7b5d50dc4186ed5ec744629d54984
Andy Galdi <agaldi@zelusanalytics.com>
2022-10-19 15:48:54 -0400

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Andy Galdi on 10/19/22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Andy Galdi on 10/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
